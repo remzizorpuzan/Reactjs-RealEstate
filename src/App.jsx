743dd9034090071dbd8b61b8af6d77d0230d3ee5
remzizorpuzan <remzizorpuzan@outlook.com>
2024-02-01 11:17:02 +0300

    Navigation/tabicon-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/homepage/Homepage";
 import Contact from "./pages/contact/Contact";
@@ -13,22 +13,20 @@ function App() {
   return (
     <Router>
       <Topbar />
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route exact path="/hizmetlerimiz">
-          <Services />
-        </Route>
-        <Route exact path="/iletisim">
-          <Contact />
-        </Route>
-        <Route exact path="/hakkimizda">
-          <Aboutus />
-        </Route>
-        <Route exact path="/ilanlar" component={PropertyListPage} />
-        <Route path="/ilanlar/:id" component={PropertyDetailsPage} />
-      </Switch>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route exact path="/hizmetlerimiz">
+        <Services />
+      </Route>
+      <Route exact path="/iletisim">
+        <Contact />
+      </Route>
+      <Route exact path="/hakkimizda">
+        <Aboutus />
+      </Route>
+      <Route exact path="/ilanlar" component={PropertyListPage} />
+      <Route path="/ilanlar/:id" component={PropertyDetailsPage} />
       <Footer />
     </Router>
   );
